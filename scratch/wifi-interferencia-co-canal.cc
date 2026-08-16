f053fb57060b1b13e1210ef2ad50a6343664981c
Marcondes-Amarante <117780345+Marcondes-Amarante@users.noreply.github.com>
2026-08-16 10:21:32 -0300

    feat: estabelecimento da comunicacao UDP

--- scratch/wifi-interferencia-co-canal.cc
+++ scratch/wifi-interferencia-co-canal.cc
@@ -115,9 +115,75 @@ int main(){
     Ipv4InterfaceContainer ipsRedeB = address.Assign(interfacesRedeB);
 
 
-    //criando aplicação de ping para teste da comunicação intra-rede
-    V4PingHelper ping1(ipsRedeA.GetAddress(0));
+    //config da aplicação UDP
+    //Ap das redes como server UDP
+    uint16_t portaA = 5000;
+    UdpServerHelper serverA(portaA);
+    ApplicationContainer serverAppA = serverA.Install(ap1.Get(0));
 
+    uint16_t portaB = 5001;
+    UdpServerHelper serverB(portaB);
+    ApplicationContainer serverAppB = serverB.Install(ap2.Get(0));
+
+    ApplicationContainer allAppServers;
+    allAppServers.Add(serverAppA);
+    allAppServers.Add(serverAppB);
+
+    allAppServers.Start(Seconds(1.0));
+    allAppServers.Stop(Seconds(10.0));
+
+    //Stations como clients
+    UdpClientHelper clientA(ipsRedeA.GetAddress(1), portaA);
+    clientA.SetAttribute("MaxPackets", UintegerValue(1000));
+    clientA.SetAttribute("Interval", TimeValue(Seconds(0.01)));
+    clientA.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientAppA = clientA.Install(sta1.Get(0));
+
+    UdpClientHelper clientB(ipsRedeB.GetAddress(1), portaB);
+    clientB.SetAttribute("MaxPackets", UintegerValue(1000));
+    clientB.SetAttribute("Interval", TimeValue(Seconds(0.01)));
+    clientB.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientAppB = clientB.Install(sta2.Get(0));
+    
+    ApplicationContainer allAppClients;
+    allAppClients.Add(clientAppA);
+    allAppClients.Add(clientAppB);
+
+    allAppClients.Start(Seconds(2.0));
+    allAppClients.Stop(Seconds(10.0));
+
+    //controle da simulação
+    Simulator::Stop(Seconds(11.0));
+    Simulator::Run();
+
+    //verificação de funcionamento dos nós
+    std::cout << "IP STA1: "
+          << ipsRedeA.GetAddress(0) << std::endl;
+
+    std::cout << "IP AP1: "
+            << ipsRedeA.GetAddress(1) << std::endl;
+
+    std::cout << "IP STA2: "
+            << ipsRedeB.GetAddress(0) << std::endl;
+
+    std::cout << "IP AP2: "
+            << ipsRedeB.GetAddress(1) << std::endl;
+
+    std::cout << "Pacotes recebidos pelo AP1: "
+          << allAppServers.Get(0)
+                  ->GetObject<UdpServer>()
+                  ->GetReceived()
+          << std::endl;
+
+    std::cout << "Pacotes recebidos pelo AP2: "
+            << allAppServers.Get(1)
+                    ->GetObject<UdpServer>()
+                    ->GetReceived()
+            << std::endl;
+
+    Simulator::Destroy();
 
     return 0;
 }
